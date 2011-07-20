e83cfcc8db860ee699911219cd723fb7de6f805f
Arif Shakoor <shakoor.arif@gmail.com>
2011-07-20 22:42:21 +0600

    testing completed for list of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
  
   def index
     @title = "All users"
-    @users = User.all
+    @users = User.paginate(:page => params[:page])
   end
 
   def show
