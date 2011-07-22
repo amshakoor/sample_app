4a8190bee0c1d99b2e841f01a5102c2cada5363e
Arif Shakoor <shakoor.arif@gmail.com>
2011-07-22 12:25:42 +0600

    checkpoint for microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page => params[:page])
     @title = @user.name
   end
 
@@ -32,11 +33,6 @@ class UsersController < ApplicationController
   def index
     @title = "All users"
     @users = User.paginate(:page => params[:page])
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @title = @user.name
   end
 
   def update
