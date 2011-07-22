ecb7a3c9303c96acb800bd36f61f713b3d7b4feb
Arif Shakoor <shakoor.arif@gmail.com>
2011-07-22 13:05:16 +0600

    Added user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 SampleApp::Application.routes.draw do
   get "sessions/new"
+  resources :users
   resources :sessions, :only => [:new, :create, :destroy]
+  resources :microposts, :only => [:create, :destroy]
 
   match '/signup',  :to => 'users#new'
   match '/signin',  :to => 'sessions#new'
   match '/signout', :to => 'sessions#destroy'
-
-  resources :users
 
   match '/signup',  :to => 'users#new'
 
