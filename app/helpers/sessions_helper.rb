aad3b48660bd60332ac2c858d3d13dca65ce3f0e
Arif Shakoor <shakoor.arif@gmail.com>
2011-07-24 22:40:56 +0600

    Added user following

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -33,11 +33,6 @@ module SessionsHelper
     store_location
     redirect_to signin_path, :notice => "Please sign in to access this page."
   end
-
-  def deny_access
-    store_location
-    redirect_to signin_path, :notice => "Please sign in to access this page."
-  end
   
   def redirect_back_or(default)
     redirect_to(session[:return_to] || default)
